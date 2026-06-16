7b53dcd4e72c154710644ce3361e08b8cbc76620
eneascv27 <eneascv27@gmail.com>
2026-06-15 23:14:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.post("/send-email", async (req, res) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
+      port: 465,
+secure: true,
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASS,
